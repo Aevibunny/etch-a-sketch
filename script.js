7e293625e2b18d760c4b0963147772732272417b
Aevibunny <137563523+Aevibunny@users.noreply.github.com>
2023-10-04 19:12:58 -0700

    Add color selection buttons and functionality

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const divContainer = document.querySelector('#divContainer')
-const singleDiv = document.createElement('div');
-singleDiv.style.border = '2px solid blue';
-singleDiv.style.flex = '1 0';
-const rowDiv = document.createElement('div')
 
+//Color selection
 let colorSelection = 'colorBlack';
+const blackButton = document.querySelector('#blackButton');
+const rainbowButton = document.querySelector('#rainbowButton');
+const eraserButton = document.querySelector('#eraserButton');
 
-const makeBlack = (e) => {
+blackButton.addEventListener('click', () => {
+    colorSelection = 'colorBlack';
+})
+
+rainbowButton.addEventListener('click', () => {
+    colorSelection = 'colorRainbow';
+})
+
+eraserButton.addEventListener('click', () => {
+    colorSelection = 'colorEraser';
+})
+
+const makeColor = (e) => {
     let targetDiv = e.target;
     if (colorSelection === 'colorBlack') {
         targetDiv.style.backgroundColor = 'black';
-    }
+    } else if (colorSelection === 'colorEraser') {
+        targetDiv.style.backgroundColor = 'white';
+    } else if (colorSelection === 'colorRainbow') {
+        const randomColor = 
+    Math.floor(Math.random()*16777215).toString(16);
+        targetDiv.style.backgroundColor = "#" + randomColor;
+    } 
 };
 
 //Generate Divs function
@@ -27,31 +45,22 @@ divContainer.innerHTML = '';
         const cell = document.createElement("div"); 
         cell.id = "sketchBox"; 
         cell.style.flex = '1';
-        cell.addEventListener('mouseover', makeBlack);
+        cell.addEventListener('mouseover', makeColor);
         row.appendChild(cell); 
     } 
 }
 };
 
-// Default load
+//Default load
 window.onload = generateDivGrid(16);
 
 //Slider
 var slider = document.getElementById("myRange");
 var output = document.getElementById("sizeSlider");
-output.textContent = slider.value;
+output.textContent = slider.value + " x " + slider.value;
 
 slider.oninput = function() {
     output.textContent = this.value + " x " + this.value;
     generateDivGrid(this.value);
   }
 
-// Click effect
-// const sketchBox = document.querySelector('#sketchBox')
-
-// const makeBlack = (e) => {
-//     let targetDiv = e.target;
-//     targetDiv.style.backgroundColor = 'red';
-// }
-
-// sketchBox.addEventListener('click', makeBlack);
