bc66b185586f351965a96ab11c789356dd151670
Aevibunny <137563523+Aevibunny@users.noreply.github.com>
2023-10-04 18:58:21 -0700

    add black mouseover effect

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ singleDiv.style.border = '2px solid blue';
 singleDiv.style.flex = '1 0';
 const rowDiv = document.createElement('div')
 
+let colorSelection = 'colorBlack';
+
+const makeBlack = (e) => {
+    let targetDiv = e.target;
+    if (colorSelection === 'colorBlack') {
+        targetDiv.style.backgroundColor = 'black';
+    }
+};
+
 //Generate Divs function
 const generateDivGrid = (amount) => {
 divContainer.innerHTML = '';
@@ -16,13 +25,16 @@ divContainer.innerHTML = '';
      divContainer.appendChild(row);
      for (let x = 0; x < amount; x++) {
         const cell = document.createElement("div"); 
-        cell.className = "sketchBox"; 
-        cell.style.border = '1px solid blue';
+        cell.id = "sketchBox"; 
         cell.style.flex = '1';
+        cell.addEventListener('mouseover', makeBlack);
         row.appendChild(cell); 
     } 
 }
 };
+
+// Default load
+window.onload = generateDivGrid(16);
 
 //Slider
 var slider = document.getElementById("myRange");
@@ -30,8 +42,16 @@ var output = document.getElementById("sizeSlider");
 output.textContent = slider.value;
 
 slider.oninput = function() {
-    output.textContent = this.value;
+    output.textContent = this.value + " x " + this.value;
     generateDivGrid(this.value);
   }
 
 // Click effect
+// const sketchBox = document.querySelector('#sketchBox')
+
+// const makeBlack = (e) => {
+//     let targetDiv = e.target;
+//     targetDiv.style.backgroundColor = 'red';
+// }
+
+// sketchBox.addEventListener('click', makeBlack);
